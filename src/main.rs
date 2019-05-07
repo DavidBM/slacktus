3e27794f17feb1c4eb4963816690e6dfa8b25a4c
David Bonet Montes <webbonet@gmail.com>
2019-05-07 20:01:07 +0200

    clippy & rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
-extern crate clap;
-extern crate reqwest;
 extern crate chrono;
+extern crate reqwest;
 
-use reqwest::header::AUTHORIZATION;
-use clap::{Arg, App, ArgMatches};
-use serde::{Serialize, Deserialize};
 use chrono::{Duration, Utc};
-
+use clap::{App, Arg, ArgMatches};
+use reqwest::header::AUTHORIZATION;
+use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 struct Profile {
-	status_text: Option<String>,
-	status_emoji: Option<String>,
-	status_expiration: u32,
+    status_text: Option<String>,
+    status_emoji: Option<String>,
+    status_expiration: u32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ProfileRequest {
-	profile: Profile
+    profile: Profile,
 }
 
 const SLACK_URL: &str = "https://slack.com/api/users.profile.set";
 
 fn main() {
-	let matches = App::new("Slacktus - Change your slack status with one command.")
+    let matches = App::new("Slacktus - Change your slack status with one command.")
 	.version("1.0")
 	.author("David Bonet <dbmontes@gmail.com>")
 	.about("It updates your slack status in one command. Perfect for pomodoro apps and similar.")
@@ -63,64 +61,79 @@ fn main() {
 		.takes_value(true))
 	.get_matches();
 
-	let mut profile_data = Profile {status_text: None, status_emoji: None, status_expiration: 0};
+    let mut profile_data = Profile {
+        status_text: None,
+        status_emoji: None,
+        status_expiration: 0,
+    };
 
-	if let Some(text) = matches.value_of("status") {
-		profile_data.status_text = Some(text.into());
-	}
-	
-	let url_to_call = if let Some(url) = matches.value_of("api_url") {
-		url
-	} else {
-		SLACK_URL
-	};
+    if let Some(text) = matches.value_of("status") {
+        profile_data.status_text = Some(text.into());
+    }
 
-	if let Some(text) = matches.value_of("emoji") {
-		profile_data.status_emoji = Some(text.into());
-	}
+    let url_to_call = if let Some(url) = matches.value_of("api_url") {
+        url
+    } else {
+        SLACK_URL
+    };
 
-	profile_data.status_expiration = get_expiration(&matches, 0);
+    if let Some(text) = matches.value_of("emoji") {
+        profile_data.status_emoji = Some(text.into());
+    }
 
-	let token = matches.value_of("token").unwrap_or("--token=TOKEN is required");
+    profile_data.status_expiration = get_expiration(&matches, 0);
 
-	let profile_request = ProfileRequest { profile: profile_data };
+    let token = matches
+        .value_of("token")
+        .unwrap_or("--token=TOKEN is required");
 
-	let request = reqwest::Client::new()
-	.post(url_to_call)
-	.header(AUTHORIZATION, format!("Bearer {}", token))
-	.json(&profile_request);
+    let profile_request = ProfileRequest {
+        profile: profile_data,
+    };
 
-	if matches.is_present("verbose") {
-		println!("Request: {:?}", request);
-		println!("Request body: {:?}", profile_request);
-	}
+    let request = reqwest::Client::new()
+        .post(url_to_call)
+        .header(AUTHORIZATION, format!("Bearer {}", token))
+        .json(&profile_request);
 
-	let result: Result<_, _> = request.send();
+    if matches.is_present("verbose") {
+        println!("Request: {:?}", request);
+        println!("Request body: {:?}", profile_request);
+    }
 
-
-	if matches.is_present("verbose") {
-		println!("Response: {:?}", result);
-	}
+    let result: Result<_, _> = request.send();
+    if matches.is_present("verbose") {
+        println!("Response: {:?}", result);
+    }
 }
 
-fn get_expiration(matches: &ArgMatches, default: u32)->  u32 {
-	let mut final_expiration: u32 = default;
+fn get_expiration(matches: &ArgMatches, default: u32) -> u32 {
+    let mut final_expiration: u32 = default;
 
-	if let Some(text) = matches.value_of("expiration") {
-		final_expiration = if let Ok(expiration_parsed) = text.parse::<u32>() {
-			expiration_parsed
-		} else {
-			panic!("expiration needs to be a number in UNIX timestamp, I couldn't parse this: {}", text);
-		}
-	}
+    if let Some(text) = matches.value_of("expiration") {
+        final_expiration = if let Ok(expiration_parsed) = text.parse::<u32>() {
+            expiration_parsed
+        } else {
+            panic!(
+                "expiration needs to be a number in UNIX timestamp, I couldn't parse this: {}",
+                text
+            );
+        }
+    }
 
-	if let Some(text) = matches.value_of("duration") {
-		final_expiration = if let Ok(duration_parsed) = text.parse::<i32>() {
-			Utc::now().checked_add_signed(Duration::seconds(duration_parsed as i64)).unwrap().timestamp() as u32
-		} else {
-			panic!("duration needs to be a number, I couldn't parse this: {}", text);
-		}
-	};
+    if let Some(text) = matches.value_of("duration") {
+        final_expiration = if let Ok(duration_parsed) = text.parse::<i32>() {
+            Utc::now()
+                .checked_add_signed(Duration::seconds(i64::from(duration_parsed)))
+                .unwrap()
+                .timestamp() as u32
+        } else {
+            panic!(
+                "duration needs to be a number, I couldn't parse this: {}",
+                text
+            );
+        }
+    };
 
-	final_expiration
+    final_expiration
 }
